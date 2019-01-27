7360a831fceed952b853f94f209a69a6478df5dd
박성출 <pschone@hotmail.com>
2019-01-27 21:12:01 +0900

    Linked Server 코드 정리

--- check1_V2.sql
+++ check1_V2.sql
@@ -779,21 +779,22 @@ SELECT
   --,modify_date
 FROM #LinkInfo
 
-IF @@rowcount = 0 
-BEGIN 
-        PRINT '** No link server connections Detection of ** '
-END
-ELSE
-BEGIN
-    PRINT '--##  List all Linked Servers and their associated login'
-    SELECT ss.server_id ,ss.name ,'Server ' = Case ss.Server_id   when 0 then 'Current Server'   else 'Remote Server'   end
+-- IF @@rowcount = 0 
+-- BEGIN 
+--         PRINT '** No link server connections Detection of ** '
+-- END
+-- ELSE
+-- BEGIN
+PRINT '--##  List all Linked Servers and their associated login'
+SELECT ss.server_id ,ss.name ,'Server ' = Case ss.Server_id   when 0 then 'Current Server'   else 'Remote Server'   end
     ,ss.data_source,ss.product   ,ss.provider  ,ss.catalog  ,'Local Login ' = case sl.uses_self_credential   when 1 then 'Uses Self Credentials'
     else ssp.name end ,'Remote Login Name' = sl.remote_name ,'RPC Out Enabled'    = case ss.is_rpc_out_enabled when 1 then 'True'
     else 'False' end ,'Data Access Enabled' = case ss.is_data_access_enabled when 1 then 'True' else 'False' end
-    ,ss.modify_date FROM sys.Servers ss  
+    ,ss.modify_date
+FROM sys.Servers ss  
     LEFT JOIN sys.linked_logins sl ON ss.server_id = sl.server_id
     LEFT JOIN sys.server_principals ssp ON ssp.principal_id = sl.local_principal_id
-END
+--END
 ------------------------------------------------------------------------
 PRINT '--##  Script out the Logon Triggers of the server, if any exists'
 SELECT SSM.definition FROM sys.server_triggers AS ST JOIN sys.server_sql_modules AS SSM ON ST.object_id = SSM.object_id
